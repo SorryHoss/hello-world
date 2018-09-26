37197aa84bff0a015e246209c3b26d681b983cdb
Jeremy <jheide@evolveacademy.io>
2018-09-25 21:51:37 -0600

    Actual Game

---
+++ fruit_basket.py
@@ -0,0 +1,23 @@
+#Fruit Basket Fun with Evolve
+import random
+play="y"
+
+print(".\nWhat is your Name?")
+player=input(">")
+while play=="y":
+    print("Let's play a game!\nGuess what fruit I am thinking of, "+player+"!")
+    fruit=random.choice(["orange","apple","pear","peach","grape"])
+    if fruit=="orange" or fruit=="apple" or fruit=="grape" or fruit=="peach":
+        ana="an"
+    if fruit=="pear":
+        ana="a"
+    answer=input(">")
+    answer=answer.lower()
+    if answer!=fruit:
+        print("Wrong.  I was thinking of "+ana+" "+fruit+".")
+    else:
+        print("Correct!")
+    play=input("Play again? yes or no\n>")
+    play=play.lower()
+    if play=="yes":
+        play="y"
